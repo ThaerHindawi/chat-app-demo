572852c75a0b559628d610aea724467421256a51
Thaer <thaer@gmail.com>
2021-05-27 20:38:56 +0300

    connect to mysql

--- app.js
+++ app.js
@@ -10,22 +10,60 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+const mysql = require('mysql');
+
+// create a connection
+const con = mysql.createConnection({
+    user: "root",
+    password: "",
+    database: "public_chat"
+});
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected To The Datebase!');
+
+    // Create Database
+    con.query("CREATE DATABASE IF NOT EXISTS public_chat", (err, result) => {
+        if (err) throw err;
+        console.log("Datebase Created");
+    });
+
+    // Create Table
+    const sql = "CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), body VARCHAR(255), createdAt datetime)";
+    con.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("table has been created...");
+    });
+})
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
+let name;
+
 app.get('/', (req, res) => {
-    res.redirect('/chat.html');
+    // Select All
+    const sql = "SELECT * FROM messages";
+    con.query(sql, function (err, result, fields) {
+        if(err) throw err;
+        console.log(result);
+        res.render('chat', {
+            name: name,
+            messages: result
+        });
+    });    
 });
 
 // Listen on connections
 io.on("connection", socket => {
-    const name = 'user' + Math.floor(Math.random() * 1000000);
+    name = 'user' + Math.floor(Math.random() * 1000000);
     socket.on("disconnect", () => {
         console.log("user disconnected");
     });
 
-    console.log(`${socket.id} is connected`);
+    console.log(`${name} is connected`);
 
     socket.on('message', async (message) => {
         console.log(message);
@@ -36,8 +74,14 @@ io.on("connection", socket => {
 
         if (message.body) {
 
-            try {
-                // save message in database here if you want
+            try {                
+                // Insert Into Table
+                const sql = `INSERT INTO messages (name, body, createdAt) VALUES ('${name}', '${message.body}', '${formatted_date}')`;
+                console.log(sql);
+                con.query(sql, function (err, result) {
+                    if (err) throw err;
+                    console.log("Data inserted into table...");
+                });
 
                 io.emit('newMessage', {
                     name: name,
